60bc2059bce4cf4f8d7fbc07a3dcd44e12248097
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2023-10-02 10:09:20 -0500

    added default for material ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
+import React from "react";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { CssBaseline, ThemeProvider, Typography } from "@mui/material";
+import theme from "./config/theme";
+
 function App() {
   return (
-    <div className="App">
-      <p>Jide</p>
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Typography variant="h1">Jide</Typography>
+      </ThemeProvider>
+    </>
   );
 }
 
